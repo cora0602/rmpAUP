5a1398042cc26662fa99900075f3ae0a4a14cc9b
cora0602 <corachen0602@gmail.com>
2023-11-13 11:18:44 +0100

    try to establish database

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port =3000; 
 
+app.set('view engine', 'ejs'); 
+
 const database = new pg.Client({
     user: "postgres",
     host: "localhost",
@@ -21,12 +23,20 @@ app.use(express.static('public'));
 // Routes
 // Main page
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Rate My Professor' });
-});
+    res.render('index.ejs', { title: 'Rate My Professor' });
+
+  });
+  
 
 // Search for professors
-app.get('/search/professor', (req, res) => {
-    res.render('index', { title: 'Rate My Professor' });
+app.get('/professor', async(req, res) => {
+    try {
+        const result = await database.query('SELECT * FROM professors');
+        res.json(result.rows);
+      } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).send('Internal Server Error');
+      }
   });
 
 // Start the server
