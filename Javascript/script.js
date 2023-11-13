0b5da096061ad6169b75f77694e9246557a62d93
cora0602 <corachen0602@gmail.com>
2023-11-13 10:10:16 +0100

    search not working

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,11 +1,15 @@
-document.getElementById('professorSearchForm').addEventListener('submit', function (event) {
+document.getElementById('searchForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Get the professor name from the input field
     const professorName = document.getElementById('professorName').value.trim();
 
+    // Check if the professor name is not empty
     if (professorName) {
+        // Construct the URL for your own search results page
         const searchResultsUrl = 'results.html?professor=' + encodeURIComponent(professorName);
+
+        // Redirect to your search results page
         window.location.href = searchResultsUrl;
     } else {
         // Display an error message or take appropriate action
