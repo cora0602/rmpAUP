c5c05ed38c35efd7cd2e92de324188e6b0e8f1a6
cora0602 <corachen0602@gmail.com>
2023-11-13 09:53:16 +0100

    test2

---
+++ Javascript/script.js
@@ -0,0 +1,14 @@
+document.getElementById('professorSearchForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get the professor name from the input field
+    const professorName = document.getElementById('professorName').value.trim();
+
+    if (professorName) {
+        const searchResultsUrl = 'your_search_results_page.html?professor=' + encodeURIComponent(professorName);
+        window.location.href = searchResultsUrl;
+    } else {
+        // Display an error message or take appropriate action
+        alert('Please enter a professor name.');
+    }
+});
